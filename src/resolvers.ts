c752299f5016218db7bb780f03495b2bc284c4bd
tuannm99 <tuannm@foobla.com>
2024-05-07 00:13:01 +0700

    models

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -5,9 +5,18 @@ export const resolvers: Resolvers = {
         featuredPlaylists: (_, __, {dataSources}) => {
             return dataSources.spotifyAPI.getFeaturedPlaylists()
         },
-
         playlist: (_, {id}, {dataSources}) => {
             return dataSources.spotifyAPI.getPlaylist(id)
         }
+    },
+    Playlist: {
+        tracks: async ({tracks, id}, _, {dataSources}) => {
+            return tracks.items
+                ? tracks.items.map(({track}) => track)
+                : dataSources.spotifyAPI.getTracks(id)
+        }
+    },
+    Track: {
+        durationMs: (parent) => parent.duration_ms
     }
 }
