a170a6afdf23f4eb81487c2f028fab3028e2c852
tuannm99 <tuannm@foobla.com>
2024-05-07 23:25:33 +0700

    final

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -9,6 +9,31 @@ export const resolvers: Resolvers = {
             return dataSources.spotifyAPI.getPlaylist(id)
         }
     },
+    Mutation: {
+        async addItemsToPlaylist(_, {input}, {dataSources}) {
+            try {
+                const response = await dataSources.spotifyAPI.addItemsToPlaylist(input)
+                if (response.snapshot_id) {
+                    const playlist = await dataSources.spotifyAPI.getPlaylist(response.snapshot_id)
+                    return {
+                        code: 200,
+                        success: true,
+                        message: 'Tracks added to playlist!',
+                        playlistId: response.snapshot_id
+                    }
+                } else {
+                    throw Error('snapshot_id property not found')
+                }
+            } catch (e) {
+                return {
+                    code: 500,
+                    success: false,
+                    message: `Something went wrong: ${e}`,
+                    playlistId: null
+                }
+            }
+        }
+    },
     Playlist: {
         tracks: async ({tracks, id}, _, {dataSources}) => {
             return tracks.items
@@ -18,5 +43,10 @@ export const resolvers: Resolvers = {
     },
     Track: {
         durationMs: (parent) => parent.duration_ms
+    },
+    AddItemsToPlaylistPayload: {
+        playlist: ({playlistId}, _, {dataSources}) => {
+            return dataSources.spotifyAPI.getPlaylist(playlistId)
+        }
     }
 }
